083540bf3855a67d2d03d34fcbd11706f14951ab
Mohsen Naderi <Mohsen.Naderi.Boldajy@gmail.com>
2025-04-09 17:29:02 +0330

    main.py

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 import asyncio
-import nest_asyncio
+from telegram.ext import Application
 from bot import start, button_handler, handle_text
-from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters
 
 async def main():
     application = Application.builder().token("8029342172:AAEdx4O9KAYvjEJUXLeKzim5MotqMZURMOs").build()
 
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CallbackQueryHandler(button_handler))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
+    application.add_handler(start)
+    application.add_handler(button_handler)
+    application.add_handler(handle_text)
 
     print("✅ ربات تحلیل‌گر راه‌اندازی شد.")
-    await application.initialize()
-    await application.start()
-    await application.updater.start_polling()
-    await application.updater.idle()
+    await application.run_polling(allowed_updates=["message", "callback_query"])
 
 if __name__ == "__main__":
+    import nest_asyncio
     nest_asyncio.apply()
-    asyncio.get_event_loop().run_until_complete(main())
+    asyncio.run(main())
