52e8a8942cc3584b46133dcf089e3e459a700dd1
Mohsen Naderi <Mohsen.Naderi.Boldajy@gmail.com>
2025-04-09 17:45:00 +0330

    main.py

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ from bot import start, button_handler, handle_text
 async def main():
     application = Application.builder().token("8029342172:AAEdx4O9KAYvjEJUXLeKzim5MotqMZURMOs").build()
 
-    application.add_handler(start)
-    application.add_handler(button_handler)
-    application.add_handler(handle_text)
+    from telegram.ext import CommandHandler, CallbackQueryHandler, MessageHandler, filters
+
+application.add_handler(CommandHandler("start", start))
+application.add_handler(CallbackQueryHandler(button_handler))
+application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
+
 
     print("✅ ربات تحلیل‌گر راه‌اندازی شد.")
     await application.run_polling(allowed_updates=["message", "callback_query"])
